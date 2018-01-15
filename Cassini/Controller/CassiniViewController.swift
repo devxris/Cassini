2ea506782dda279ae7ce543500bbf96b6641b46a
devxris <chuwei0915@gmail.com>
2018-01-15 14:24:41 +0800

    Lecture 10: Adapt both iPhone and iPad

--- Cassini/Controller/CassiniViewController.swift
+++ Cassini/Controller/CassiniViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class CassiniViewController: UIViewController {
+class CassiniViewController: UIViewController, UISplitViewControllerDelegate {
+	
+	// MARK: UISplitViewControllerDelegate
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		splitViewController?.delegate = self
+	}
+	
+	func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+		if let imageViewController = secondaryViewController as? ImageViewController {
+			if imageViewController.imageURL == nil {
+				return false
+			}
+		}
+		return true
+	}
 
 	// MARK: Navigations
 	
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		if let identifier = segue.identifier {
 			if let url = DemoURLs.NASA[identifier] {
-				if let imageVC = segue.destination as? ImageViewController {
+				if let imageVC = segue.destination.contents as? ImageViewController {
 					imageVC.imageURL = url
 					imageVC.title = (sender as? UIButton)?.currentTitle
 				}
 			}
 		}
+	}
+}
+
+extension UIViewController {
+	var contents: UIViewController {
+		if let navCon = self as? UINavigationController {
+			return navCon.visibleViewController ?? navCon
+		}
+		return self
 	}
 }
